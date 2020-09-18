9159f6734a0a4c00e12483aa4206536f152b0e69
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-09-18 15:48:49 +0300

    import ReactiveForms

--- src/app/basic-form/basic-form.module.ts
+++ src/app/basic-form/basic-form.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicFormComponent } from './basic-form/basic-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [BasicFormComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule
   ],
-  exports: [
+  exports: [ 
+    BasicFormComponent
   ]
 })
 export class BasicFormModule { }
--- src/app/basic-form/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -7,12 +7,43 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss']
 })
 export class BasicFormComponent implements OnInit {
-  form: FormGroup
+  myForm: FormGroup
+  programmingLanguages = ["Python","Java","JS"];
 
   constructor() { }
 
   ngOnInit() {
+    this.myForm = new FormGroup({
+      firstName: new FormControl("",[Validators.required,
+        Validators.minLength(3)]),
+      lastName: new FormControl("",[Validators.required,
+        Validators.minLength(3)]),
+      isExperienced: new FormControl("", Validators.required),
+      angular: new FormControl("", Validators.required),
+      favouriteLanguage: new FormControl("",Validators.required),
+      jsversion: new FormControl("",Validators.required)
+    });
 
+
+
+    this.myForm.get('favouriteLanguage').valueChanges.subscribe( (value: string) => {
+      const jsversion =this.myForm.get('jsversion');
+      if (value === "JS") {
+        jsversion.setValidators(Validators.required);
+        } else {
+          jsversion.clearValidators();
+        }
+        jsversion.updateValueAndValidity();
+      })
   }
 
+  formSubmit(form:FormGroup){
+    if (!form.valid) {
+      return;
+    }
+    console.log(form.value);
+  }
+  
 }
+
+
